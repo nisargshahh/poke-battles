031085b45019aebf4afb18a9e8d15678849edb2c
Nisarg Shah <devnisargshah@gmail.com>
2025-01-07 12:12:20 +0530

    minor changes

--- battleSimulator.js
+++ battleSimulator.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '', 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.use(express.json({ limit: "50mb" }));
